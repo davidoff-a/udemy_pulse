f6cb4394fba9adff39d314f5171ad8e1f9c07382
aliaksei davydau <davidoff.aleksei@gmail.com>
2021-01-05 15:42:40 +0300

    refactor: solve some PROBLEMS with styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,7 @@
 $(document).ready(function () {
 	$('.carousel__inner').slick({
-		speed: 1200,
-		adaptiveHeight: true,
-		prevArrow:
-			'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-		nextArrow:
-			'<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-		responsive: [
-			{
-				breakpoint: 992,
-				settings: {
-					dots: true,
-					arrows: false,
-				},
-			},
-		],
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
 	});
 });
