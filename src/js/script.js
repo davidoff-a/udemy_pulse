e321d52aa98e1897c47e33f2406072297f31ac62
aliaksei davydau <davidoff.aleksei@gmail.com>
2021-01-06 12:05:02 +0300

    refactor: slick slider works properly if project starts with index.html, not any live server

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-	$('.carousel__inner').slick();
+	$('.carousel__inner').slick({
+		speed: 1200,
+		adaptiveHeight: true,
+		autoplay: true,
+		autoplaySpeed:  2000,
+		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/chevron-left-solid.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/chevron-right-solid.png"></button>'
+	});
 });
