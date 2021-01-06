2e1e0059763b48d44b07ac8cebed7a93dac241ba
aliaksei davydau <davidoff.aleksei@gmail.com>
2021-01-06 13:08:45 +0300

    feature: add catalog-tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 1200,
-		adaptiveHeight: true,
+		adaptiveHeight: false,
 		autoplay: true,
-		autoplaySpeed:  2000,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/chevron-left-solid.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/chevron-right-solid.png"></button>'
+		autoplaySpeed: 2000,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="icons/slider/chevron-left-solid.png"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="icons/slider/chevron-right-solid.png"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+
 	});
 });
