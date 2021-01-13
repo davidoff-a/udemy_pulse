e7bf9daa9f0290ccd953d57bf500b0eca874deba
aliaksei davydau <davidoff.aleksei@gmail.com>
2021-01-13 13:53:50 +0300

    Feature: Add scripts/actions to modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -43,7 +43,21 @@ $(document).ready(function () {
 				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
 			});
 		});
-	};
+	}
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
+
+	//modal
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+	});
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal_descr').text($('.catalog-item_subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
 });
